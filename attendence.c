7ee2d2a87015083fafa8c1046b2396b4659817dc
siddharth-davane <davanesiddharth94@gmail.com>
2025-12-28 23:11:18 +0530

    Release V1.0 Added all basic functionality [NEW] Added 'Group' output format [NEW] Added 'Save to a file' function

--- attendence.c
+++ attendence.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-  printf("Attendance Counter\ncontrols:\ny: Present\nn: Absent\n");
+  printf("Attendance Counter V1.0\ncontrols:\ny: Present\nn: Absent\n");
   printf("j/J: jump to roll.no\n!: Finish\n Max students: 100");
 
   short int presenty[100] ={0};// fill array with 0, removes old unknown values in the memory
   const int MAX_PRESENTY= 99;
-  int jump_no, highest_rollno_reached=0, current_rollno = 0;
-  char command;
+  int jump_no, highest_rollno_reached=0, current_rollno = 0, outputFormat;
+  char command, saveToDisk, filename[128];
 
 
   while (current_rollno<=MAX_PRESENTY){
@@ -42,23 +42,94 @@ int main(){
 
   }
 
+  printf("\nChoose output format: \n1. List \n2. Group\nOption: ");
+  scanf("%d",&outputFormat);
+
 
   // Output Printing logic
   printf("\n\nResults :  \n------------------\n\n");
 
-  for(current_rollno=0; current_rollno<=highest_rollno_reached; current_rollno++){
-    if(presenty[current_rollno] == 1){
-      printf("Roll no %d: Present\n", current_rollno+1);
-    }
-    else if(presenty[current_rollno] == 2) {
-      printf("Roll no %d: Absent\n", current_rollno+1);
-    }
-    else{
-      // keep rollnumbers with invalid or unassigned state in output
-      printf("Roll no %d: unassigned\n", current_rollno+1);
+  if(outputFormat==1){
+    //list format
+    for(current_rollno=0; current_rollno<=highest_rollno_reached; current_rollno++){
+      if(presenty[current_rollno] == 1){
+        printf("Roll no %d: Present\n", current_rollno+1);
+      }
+      else if(presenty[current_rollno] == 2) {
+        printf("Roll no %d: Absent\n", current_rollno+1);
+      }
+      else{
+        // keep rollnumbers with invalid or unassigned state in output
+        printf("Roll no %d: unassigned\n", current_rollno+1);
+      }
     }
   }
+  else if(outputFormat==2){
+    // Group Format
+    printf("\nPresent:\n");
+    for(current_rollno=0; current_rollno<=highest_rollno_reached; current_rollno++){
+      if(presenty[current_rollno] ==1){
+        printf(" %2d", current_rollno+1);
+      }
+      else{printf("   ");}
+      if((current_rollno+1)%10==0){
+        printf("\n"); //newline every 10 numbers
+      }
+    }
+    printf("\n");//newline after output ends
+  }
+  else{printf("\nInvalid output format, no output generated");}
 
+
+  printf("\nSave this output to Disk? (y/n):");
+  scanf(" %c",&saveToDisk);
+  //Write to file if asked to.
+  //this is just same of the above program
+  //just replaced printf to file output
+  if(saveToDisk=='y'||saveToDisk=='Y'){
+    printf("\nEnter file name:");
+    scanf("%127s",filename);
+    FILE *output;
+    output = fopen(filename,"w");
+
+    // Output Printing logic
+    fprintf(output,"\n\nResults :  \n------------------\n\n");
+
+    if(outputFormat==1){
+      //list format
+      for(current_rollno=0; current_rollno<=highest_rollno_reached; current_rollno++){
+        if(presenty[current_rollno] == 1){
+          fprintf(output,"Roll no %d: Present\n", current_rollno+1);
+        }
+        else if(presenty[current_rollno] == 2) {
+          fprintf(output,"Roll no %d: Absent\n", current_rollno+1);
+        }
+        else{
+          // keep rollnumbers with invalid or unassigned state in output
+          fprintf(output,"Roll no %d: unassigned\n", current_rollno+1);
+        }
+      }
+    }
+    else if(outputFormat==2){
+      // Group Format
+      fprintf(output,"\nPresent:\n");
+      for(current_rollno=0; current_rollno<=highest_rollno_reached; current_rollno++){
+        if(presenty[current_rollno] ==1){
+          fprintf(output," %2d", current_rollno+1);
+        }
+        else{fprintf(output,"   ");}
+        if((current_rollno+1)%10==0){
+          fprintf(output,"\n"); //newline every 10 numbers
+        }
+      }
+      fprintf(output,"\n");//newline after output ends
+    }
+    else{printf("\nInvalid output format, no output generated");}
+
+    fclose(output);
+    printf("Save Successful\n\n");
+
+  }
 
   return 0;
 }
--- readme.md
+++ readme.md
@@ -1,13 +1,23 @@
 # Quick Attendance Program
+`*******  Release : V1.0  ********
 
-An program to Quickly take attendance and save it to a file.
+An program to Quickly take attendance by roll numbers
+
+This C program is designed to take upto 100 numbers, after which it outputs the data and asks to save if needed.
 
 ## Base functions
 * Take attendance of upto 100 roll numbers
 * Exit anywhere (Early quit)
+* show rollnumbers neatly
 
-## Planned features
-* Jump to a random roll number
-* Save data to a file 
-* Exiting early causes unassigned rollnumbers to be ignored in output
-* Can tell difference between unassigned vs absent
+## Extra functions
+* 2 types of output formats
+* Save the program output to a file
+  * User must provide filename
+
+## Planned 
+* In group output format: 
+  * Add optional absent numbers below present group
+
+* Warn before overwriting a file
+
